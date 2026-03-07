81732d65903be21aa5c55f73c9c45f6b9ea7d665
Y Bhavana Reddy <159869445+ybhavanareddy@users.noreply.github.com>
2026-03-08 01:04:54 +0530

    feat: setup react router and create page components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Products from './pages/Products'
+import Cart from './pages/Cart'
+import Login from './pages/Login'
+
+import { Routes, Route } from 'react-router-dom'
+
 function App() {
   return (
     <div>
       <Navbar />
       <h1>Shopsphere</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,18 @@ import React from 'react'
 function Navbar() {
   return (
     <nav>
-        <h2>Shopsphere</h2>
+        <div>
+            <h2>Shopsphere</h2>
+        </div>
+        <ul>
+            <li>Home</li>
+            <li>Products</li>
+            <li>Cart</li>
+            <li>Login</li>
+        </ul>
+        
     </nav>
+
   )
 }
 
