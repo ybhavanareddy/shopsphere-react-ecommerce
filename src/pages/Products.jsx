6fef7346240dc81a9b77f35492ae2953e7a73e90
Y Bhavana Reddy <159869445+ybhavanareddy@users.noreply.github.com>
2026-03-10 21:13:55 +0530

    feat: implement product search with controlled input

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
+import { useState } from 'react'
+
 import ProductCard from '../components/ProductCard'
 import { products } from '../services/ProductService'
 
 function Products() {
-  
+  const [search,setSearch] = useState("");
+
+  const filteredProducts = products.filter((product)=> 
+  product.title.toLowerCase().includes(search.toLowerCase()));
+
   return (
     <div>
       <h1>Products</h1>
-      {products.map(product => (
+      <input 
+        type ="text"
+        placeholder='Search products...'
+        value={search}
+        onChange={(e)=> setSearch(e.target.value)}
+      />
+      {filteredProducts.map(product => (
         <ProductCard 
           id={product.id}
           key={product.id}
