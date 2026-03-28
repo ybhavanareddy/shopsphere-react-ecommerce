2a720a185de5a4e265fd7dd62e1da32c1297a505
Y Bhavana Reddy <159869445+ybhavanareddy@users.noreply.github.com>
2026-03-28 13:34:49 +0530

    feat: implement order confirmation page with responsive UI

--- src/pages/OrderConformation.jsx
+++ src/pages/OrderConformation.jsx
@@ -2,22 +2,25 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 function OrderConformation() {
   return (
-    <div className='flex flex-col items-center justify-center h-screen'>
-        <h1 className='text-3xl font-bold mb-4'>
-            Order Placed Successfully 🎉
-        </h1>
+    <div className='bg-white shadow-lg rounded-lg p-6 sm:p-8'>
 
-        <p className='text-gray-600 mb-4'>
-            Thank you for shopping with ShopSphere.
-        </p>
+      <div className='flex flex-col items-center justify-center min-h-screen px-4 text-center'>
+          <h1 className='text-2xl sm:text-3xl font-bold mb-4'>
+              Order Placed Successfully 🎉
+          </h1>
 
-        <Link
-          to="/products"
-          className='bg-blue-900 text-white px-4 py-2 rounded hover:bg-blue-600'
-        >
-          Continue Shopping
-        </Link>
+          <p className='text-gray-600 mb-6 sm:text-base'>
+              Thank you for shopping with ShopSphere.
+          </p>
 
+          <Link
+            to="/products"
+            className='bg-blue-900 text-white px-4 py-2 rounded hover:bg-blue-600 transition'
+          >
+            Continue Shopping
+          </Link>
+
+      </div>
     </div>
   )
 }
