485b7eaaa5543b13f4f908bf337c0d9435d77ac8
Y Bhavana Reddy <159869445+ybhavanareddy@users.noreply.github.com>
2026-04-18 11:57:07 +0530

    feat: implement get products API with mock data

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,7 +1,29 @@
 //Get all products 
 
 export const getProducts = (req,res)=> {
-    res.json({
-        message:"Featured all products successfully 🎉"
-    });
+
+    const products = [
+        {
+            id:1,
+            title:"iPhone 15",
+            price:79999,
+            thumbnail:"https://m.media-amazon.com/images/I/71MlcO29QOL._SL1500_.jpg",
+            rating:4.5
+        },
+        {
+            id:2,
+            title:"Samsung Galaxy S23",
+            price:69999,
+            thumbnail:"https://m.media-amazon.com/images/I/91w+qj8n9sL._SL1500_.jpg",
+            rating:4.3
+        },
+        {
+            id:3,
+            title:"Google Pixel 7",
+            price:59999,
+            thumbnail:"https://m.media-amazon.com/images/I/71w+qj8n9sL._SL1500_.jpg",
+            rating:4.2
+        }
+    ];
+    res.json(products);
 };
