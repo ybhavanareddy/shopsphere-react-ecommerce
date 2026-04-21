822188ca5105490a9cd6855f7b0f912fcf786707
Y Bhavana Reddy <159869445+ybhavanareddy@users.noreply.github.com>
2026-04-21 13:53:58 +0530

    refactor: use env variable for API URL

--- frontend/src/services/ProductService.js
+++ frontend/src/services/ProductService.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://shopsphere-backend-xs4m.onrender.com/api/products";
+const BASE_URL = `${import.meta.env.VITE_API_URL}/api/products`;
 
 export async function fetchProducts(){
   const response = await fetch(BASE_URL);
--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://shopsphere-backend-xs4m.onrender.com/api/auth";
+const BASE_URL = `${import.meta.env.VITE_API_URL}/api/auth`;
 
 //Registr User 
 
